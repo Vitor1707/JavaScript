ea9fb0436aa1e6032c751c678947c677576cd305
Vitor Lucas soares <vitorlucasdbr13@gmail.com>
2024-10-11 14:16:24 -0300

    atualização

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,8 +1,6 @@
-var país = 'EUA'
-console.log(`Vivendo em ${país}`)
-if (país == 'Brasil') {
-    console.log('Brasileiro')
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) { /* Condição simples!  "não tem o else" */
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 }
-else {
-    console.log('Estrangeiro')
-}
+console.log(`Dirija sempre usando cinto de segurança!`)
